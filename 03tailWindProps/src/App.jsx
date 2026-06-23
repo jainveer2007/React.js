e70bc468953b9d4bff2dd7d61d67a9982c1a9540
jainveer2007 <jainveer2007@gmail.com>
2026-06-23 16:38:04 +0530

    changes added

--- 03tailWindProps/src/App.jsx
+++ 03tailWindProps/src/App.jsx
@@ -4,12 +4,46 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+// function Card10() {
+//   return (
+//     <div
+//       className="flex flex-col rounded-xl p-4"
+//       style={{
+//         border: "0.88px solid",
+//         backdropFilter: "saturate(180%) blur(14px)",
+//         background: "#ffffff0d",
+//       }}
+//     >
+//       <div>
+//         <img
+//           src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
+//           alt="nft-gif"
+//           width="350"
+//           height="350"
+//           className="rounded-xl"
+//         />
+//       </div>
+//       <div className="flex flex-col rounded-b-xl py-4">
+//         <div className="flex justify-between">
+//           <h1 className="font-RubikBold">Bored ape nft accidental</h1>
+//           <h1 className="font-bold font-RubikBold">Price</h1>
+//         </div>
+//         <div className="flex justify-between font-mono">
+//           <p>#345</p>
+//           <p>0.01</p>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='bg-green-400'>Tailwind test</h1>
+      <h1 className='bg-green-400 text-7xl text-black p-4 rounded-xl'>Tailwind test</h1>
+      {/* <Card10 /> */}
     </>
   )
 }
